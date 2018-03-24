3d5f3c37f3080e51ffd4aa7aa2be9372159989bb
Wojtascythe <wojtas75666@gmail.com>
2018-03-24 12:18:39 +0100

    Utworzone sety i gety

--- wezel.h
+++ wezel.h
@@ -1,15 +1,15 @@
 #pragma once
-
+// KLASA WĘZŁA- ELEMENTU KOLEJKI
 template <typename TYP>
 class Wezel{
 TYP wartosc;
 TYP klucz;
-Wezel<TYP> *nastepny, *poprzedni;
+Wezel<TYP> *nastepny, *poprzedni; //Wskaźniki węzła na węzeł przed nim i na węzeł po nim.
 public:
-
+//Metody poniżej są set'ami i get'ami dla powyższych prywatnych zmiennych i wksaźników
 Wezel();
 Wezel<TYP> * getNastepny() const; //wyciagnij z private'a
-Wezel<TYP> * getPoprzedni() const; 
+Wezel<TYP> * getPoprzedni() const;
 void setNastepny(Wezel<TYP> *_nastepny);
 void setPoprzedni(Wezel<TYP> *_poprzedni);
 TYP getWartosc() const;
@@ -18,3 +18,47 @@ void setWartosc(TYP _wartosc);
 void setKlucz(TYP _klucz);
 };
 
+template <typename TYP>
+Wezel<TYP>::Wezel(){
+nastepny=nullptr;
+}
+
+template <typename TYP>
+Wezel<TYP> * Wezel<TYP>::getNastepny() const{
+return nastepny;
+}
+
+template <typename TYP>
+Wezel<TYP> * Wezel<TYP>::getPoprzedni() const{
+return poprzedni;
+}
+
+template <typename TYP>
+void Wezel<TYP>::setNastepny(Wezel<TYP> *_nastepny){
+nastepny=_nastepny;
+}
+
+template <typename TYP> //tutaj podejrzane
+void Wezel<TYP>::setPoprzedni(Wezel<TYP> *_poprzedni){
+poprzedni=_poprzedni;
+}
+
+template <typename TYP>
+TYP Wezel<TYP>::getWartosc() const {
+return wartosc;
+}
+
+template <typename TYP>
+TYP Wezel<TYP>::getKlucz() const {
+return klucz;
+}
+
+template <typename TYP>
+void Wezel<TYP>::setWartosc(TYP _wartosc){
+wartosc=_wartosc;
+}
+
+template <typename TYP>
+void Wezel<TYP>::setKlucz(TYP _klucz){
+klucz=_klucz;
+}
